ea8fbba57e33ab69c467f98468430231c9bcaa19
trainig2020 <68064760+trainig2020@users.noreply.github.com>
2020-07-31 13:25:31 +0530

    Add files via upload

---
+++ Spring_Component/src/com/spring/Circle.java
@@ -0,0 +1,42 @@
+package com.spring;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import javax.annotation.Resource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Required;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Circle implements Shape{
+
+	private Points center;
+	
+	public Points getCenter() {
+		return center;
+	}
+	@Resource
+	public void setCenter(Points center) {
+		this.center = center;
+	}
+
+	@Override
+	public void draw() {
+		System.out.println("Drawing circle : ");
+		System.out.println("center is ("+ center.getX()+" , "+center.getY()+")");
+		
+	}
+	
+	@PostConstruct
+	public void initializeCircle(){
+		System.out.println("Init method");
+	}
+	
+	@PreDestroy
+	public void destruction(){
+		System.out.println("Destroy method");
+	}
+
+}
