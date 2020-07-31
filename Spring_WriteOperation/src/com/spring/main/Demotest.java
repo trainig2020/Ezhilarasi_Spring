16cd26fd27bf3c13632fba07b8cac433dc9e39f7
trainig2020 <68064760+trainig2020@users.noreply.github.com>
2020-07-31 13:31:23 +0530

    Add files via upload

---
+++ Spring_WriteOperation/src/com/spring/main/Demotest.java
@@ -0,0 +1,33 @@
+package com.spring.main;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.spring.dao.CircleDaoImpl;
+import com.spring.model.Circle;
+
+public class Demotest {
+
+	@SuppressWarnings("resource")
+	public static void main(String[] args) {
+		
+		ApplicationContext  context = new ClassPathXmlApplicationContext("spring.xml");
+		
+		CircleDaoImpl dao =  context.getBean("circleDaoImpl", CircleDaoImpl.class);
+		/*Circle circle = dao.getCircle(1);
+		System.out.println(circle.getName());*/
+		
+		//System.out.println(dao.getCircleCount());
+		
+		
+		//System.out.println(dao.getCircleById(1).getName());
+		
+		/*dao.insertCircle(new Circle(2, "second"));
+		
+		System.out.println(dao.getCircle().size());*/
+		
+		dao.createTable();
+
+	}
+
+}
